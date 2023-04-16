89b27417309f59125d3482e3d8254156b09b2e27
Hassanuabdulkadeer <92226780+Hassanuabdulkadeer@users.noreply.github.com>
2023-04-16 16:26:29 +0100

    added Sort functionality

--- search.php
+++ search.php
@@ -1,7 +1,8 @@
 <?php
-
 require_once "includes/functions.inc.php";
 require_once("includes/dbconnect.inc.php");
+
+
 
 ?>
 
@@ -42,43 +43,80 @@ require_once("includes/dbconnect.inc.php");
         <!-- Begin page content -->
         <main class="flex-shrink-0">
             <div class="container">
-                <h3>Listing Results</h3>
-                <?php
+                <div class="row">
+                    <h3>Listing Results</h3>
+                    <!-- <form method="get">
+                        <h3>Filter</h3>
+                        <div class="mb-3">
+                            <label for="price" class="form-label">Price</label>
+                            <input type="number" class="form-control" id="price" name="price">
+                        </div>
+                        <div class="mb-3">
+                            <label for="date" class="form-label">Posted Date</label>
+                            <input type="date" class="form-control" id="date" name="date">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Filter</button>
+                    </form> -->
+                    <div id="filter-form-container">
+<form method="get">
+  <div class="mb-3">
+    <label for="sort" class="form-label">Sort by</label>
+    <select class="form-select" id="sort" name="sort">
+<option value="datetime_posted DESC" <?php if($sort == "datetime_posted DESC") echo "selected"; ?>>Most Recent</option>
+      <option value="price ASC"<?php if($sort == "price ASC") echo " selected"; ?>>Price: Low to High</option>
+      <option value="price DESC"<?php if($sort == "price DESC") echo " selected"; ?>>Price: High to Low</option>
+      <option value="title ASC"<?php if($sort == "title ASC") echo " selected"; ?>>Name: A to Z</option>
+      <option value="title DESC"<?php if($sort == "title DESC") echo " selected"; ?>>Name: Z to A</option>
+    </select>
+  </div>
+  <button type="submit" class="btn btn-primary">Filter</button>
+</form>
+        </div>
 
-                $filters = "";
+                </div>
+<?php
+    $filters = "";
 
-                // Search based on term
-                if(isset($_GET['term'])){
-                    $term = $_GET['term'];
-                    $filters = " AND title LIKE '%$term%'";
-                }
+    // Sort based on option selected
+    if(isset($_GET['sort'])){
+        $sort = $_GET['sort'];
+    }
+    else{
+        $sort = "datetime_posted DESC";
+    }
 
-                $sql = "SELECT * FROM listing WHERE listing_id$filters ORDER BY datetime_posted DESC";
+    // Search based on term
+    if(isset($_GET['term'])){
+        $term = $_GET['term'];
+        $filters .= " AND title LIKE '%$term%'";
+    }
 
-                $result = mysqli_query($conn, $sql);
+    $sql = "SELECT * FROM listing WHERE 1 $filters ORDER BY $sort";
 
-                
-                if (mysqli_num_rows($result) >= 1){
-                    while($row = mysqli_fetch_assoc($result)) {
-                        echo "<a class=\"listing\" href=\"view-ad.php?listing_id=" . $row['listing_id'] . "\">";
-                        echo "<h4>" . $row['title'] . " - " . $row['artist'] . "</h4>";
-                        echo "<ul>";
-                        echo "<li>Price (Pcm): £" . $row['price'] . "</li>";
-                        echo "<li>Posted: " . $row['datetime_posted'] . "</li>";
-                        echo "</ul>";
-                        echo "</a>";
-                    }
-                }
-                else{
-                    echo "<h3>No results found</h3>";
-                    echo "<p>Your search has returned no results, please try again.</p>";
-                }
+    $result = mysqli_query($conn, $sql);
 
-                ?>
+    if (mysqli_num_rows($result) >= 1){
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "<a class=\"listing\" href=\"view-ad.php?listing_id=" . $row['listing_id'] . "\">";
+            echo "<h4>" . $row['title'] . " - " . $row['artist'] . "</h4>";
+            echo "<ul>";
+            echo "<li>Price (Pcm): £" . $row['price'] . "</li>";
+            echo "<li>Posted: " . $row['datetime_posted'] . "</li>";
+            echo "</ul>";
+            echo "</a>";
+        }
+    }
+    else{
+        echo "<h3>No results found</h3>";
+        echo "<p>Your search has returned no results, please try again.</p>";
+    }
+?>
             </div>
         </main>
 
         <!-- Bootstrap JS -->
         <script src="js/bootstrap.bundle.min.js"></script>
     </body>
+
+
 </html>
