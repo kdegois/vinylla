09cf24508969b9091c107e7bdacdaf703c7d474e
UP2013767 <up2013767@myport.ac.uk>
2023-05-07 17:20:23 +0100

    Fixed search filtering

--- search.php
+++ search.php
@@ -1,8 +1,7 @@
 <?php
+
 require_once "includes/functions.inc.php";
 require_once("includes/dbconnect.inc.php");
-
-
 
 ?>
 
@@ -40,77 +39,76 @@ require_once("includes/dbconnect.inc.php");
 
         <?php include "nav.php"?>
 
+        <?php
+
+        $term = "";
+        $filters = "";
+
+        // Get sorting method
+        if(isset($_GET['sort'])){
+            $sort = $_GET['sort'];
+        }
+        else{
+            // Default
+            $sort = "datetime_posted DESC";
+        }
+
+        // Search based on term (open user text)
+        if(isset($_GET['term'])){
+            $term = $_GET['term'];
+            // Add search term to filters
+            $filters .= " AND title LIKE '%$term%'";
+        }
+
+        ?>
+
         <!-- Begin page content -->
         <main class="flex-shrink-0">
             <div class="container">
                 <div class="row">
                     <h3>Listing Results</h3>
-                    <!-- <form method="get">
-                        <h3>Filter</h3>
-                        <div class="mb-3">
-                            <label for="price" class="form-label">Price</label>
-                            <input type="number" class="form-control" id="price" name="price">
-                        </div>
-                        <div class="mb-3">
-                            <label for="date" class="form-label">Posted Date</label>
-                            <input type="date" class="form-control" id="date" name="date">
-                        </div>
-                        <button type="submit" class="btn btn-primary">Filter</button>
-                    </form> -->
                     <div id="filter-form-container">
-<form method="get">
-  <div class="mb-3">
-    <label for="sort" class="form-label">Sort by</label>
-    <select class="form-select" id="sort" name="sort">
-<option value="datetime_posted DESC" <?php if($sort == "datetime_posted DESC") echo "selected"; ?>>Most Recent</option>
-      <option value="price ASC"<?php if($sort == "price ASC") echo " selected"; ?>>Price: Low to High</option>
-      <option value="price DESC"<?php if($sort == "price DESC") echo " selected"; ?>>Price: High to Low</option>
-      <option value="title ASC"<?php if($sort == "title ASC") echo " selected"; ?>>Name: A to Z</option>
-      <option value="title DESC"<?php if($sort == "title DESC") echo " selected"; ?>>Name: Z to A</option>
-    </select>
-  </div>
-  <button type="submit" class="btn btn-primary">Filter</button>
-</form>
-        </div>
+                        <form method="get">
+                            <input type="hidden" name="term" value="<?php echo $term; ?>"/> <!-- Existing search terms -->
+                            <div class="mb-3">
+                                <label for="sort" class="form-label">Sort by</label>
+                                <select class="form-select" id="sort" name="sort">
+                                    <option value="datetime_posted DESC" <?php if($sort == "datetime_posted DESC") echo "selected"; ?>>Most Recent</option>
+                                    <option value="price ASC"<?php if($sort == "price ASC") echo " selected"; ?>>Price: Low to High</option>
+                                    <option value="price DESC"<?php if($sort == "price DESC") echo " selected"; ?>>Price: High to Low</option>
+                                    <option value="title ASC"<?php if($sort == "title ASC") echo " selected"; ?>>Name: A to Z</option>
+                                    <option value="title DESC"<?php if($sort == "title DESC") echo " selected"; ?>>Name: Z to A</option>
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Filter</button>
+                        </form>
+                    </div>
 
                 </div>
-<?php
-    $filters = "";
 
-    // Sort based on option selected
-    if(isset($_GET['sort'])){
-        $sort = $_GET['sort'];
-    }
-    else{
-        $sort = "datetime_posted DESC";
-    }
+                <?php
 
-    // Search based on term
-    if(isset($_GET['term'])){
-        $term = $_GET['term'];
-        $filters .= " AND title LIKE '%$term%'";
-    }
+                    $sql = "SELECT * FROM listing WHERE 1 $filters ORDER BY $sort";
 
-    $sql = "SELECT * FROM listing WHERE 1 $filters ORDER BY $sort";
+                    $result = mysqli_query($conn, $sql);
 
-    $result = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($result) >= 1){
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo "<a class=\"listing\" href=\"view-ad.php?listing_id=" . $row['listing_id'] . "\">";
+                            echo "<h4>" . $row['title'] . " - " . $row['artist'] . "</h4>";
+                            echo "<ul>";
+                            echo "<li>Price (Pcm): £" . $row['price'] . "</li>";
+                            echo "<li>Posted: " . $row['datetime_posted'] . "</li>";
+                            echo "</ul>";
+                            echo "</a>";
+                        }
+                    }
+                    else{
+                        echo "<h3>No results found</h3>";
+                        echo "<p>Your search has returned no results, please try again.</p>";
+                    }
+                ?>
 
-    if (mysqli_num_rows($result) >= 1){
-        while($row = mysqli_fetch_assoc($result)) {
-            echo "<a class=\"listing\" href=\"view-ad.php?listing_id=" . $row['listing_id'] . "\">";
-            echo "<h4>" . $row['title'] . " - " . $row['artist'] . "</h4>";
-            echo "<ul>";
-            echo "<li>Price (Pcm): £" . $row['price'] . "</li>";
-            echo "<li>Posted: " . $row['datetime_posted'] . "</li>";
-            echo "</ul>";
-            echo "</a>";
-        }
-    }
-    else{
-        echo "<h3>No results found</h3>";
-        echo "<p>Your search has returned no results, please try again.</p>";
-    }
-?>
             </div>
         </main>
 
