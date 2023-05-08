21b8589fb90689f42e102d0846e4ad4ed3650405
SakriyaG <sakriyagrg17@hotmail.co.uk>
2023-05-08 19:02:09 +0100

    Basket

--- basket.php
+++ basket.php
@@ -19,12 +19,71 @@ require_once("includes/dbconnect.inc.php");
         <link href="css/main.css" rel="stylesheet">
         <!-- Sticky footer navbar -->
         <link href="css/sticky-footer-navbar.css" rel="stylesheet">
+        <!-- Custom style for search page -->
+        <style>
+            a.listing {
+                background: rgba(0,0,0,0.2);;
+                display: block;
+                color: var(--bs-body-color);
+                text-decoration: none;
+                padding: 20px;
+                margin: 20px 0;
+                border-radius: 5px;
+            }
+            a.listing:hover {
+                background: rgba(0,0,0,0.3);;
+            }
+            h3 {
+                padding-top: 20px;
+            }
+        </style>
     </head>
     <body>
 
         <?php include "nav.php"?>
 
-        <!-- CART HERE -->
+        <!-- Begin page content -->
+        <main class="flex-shrink-0">
+            <div class="container">
+
+                <?php
+                
+                    $sql = "SELECT * FROM cart WHERE user_id = 1";
+
+                    $result = mysqli_query($conn, $sql);
+
+                    if (mysqli_num_rows($result) >= 1){
+                        while($row = mysqli_fetch_assoc($result)) {
+                            $listingIdArray = $row['item_id_1'];
+                        }
+
+                    }
+
+                    echo $listingIdArray;
+
+                    $sql = "SELECT * FROM listing WHERE listing_id = 1 OR listing_id=7";
+
+                    $result = mysqli_query($conn, $sql);
+
+                    if (mysqli_num_rows($result) >= 1){
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo "<a class=\"listing\" href=\"view-ad.php?listing_id=" . $row['listing_id'] . "\">";
+                            echo "<h4>" . $row['title'] . " - " . $row['artist'] . "</h4>";
+                            echo "<ul>";
+                            echo "<li>Price (Pcm): £" . $row['price'] . "</li>";
+                            echo "<li>Posted: " . $row['datetime_posted'] . "</li>";
+                            echo "</ul>";
+                            echo "</a>";
+                        }
+                    }
+                    else{
+                        echo "<h3>No results found</h3>";
+                        echo "<p>Your search has returned no results, please try again.</p>";
+                    }
+                ?>
+
+            </div>
+        </main>
 
         <!-- Bootstrap JS -->
         <script src="js/bootstrap.bundle.min.js"></script>
